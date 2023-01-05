8d4d743b01ae5b8eabad881390dc4106060f0ab6
Bervas Yahn <yahn.bervas@etu.u-bordeaux.fr>
2023-01-05 09:24:42 +0100

    Requêtage de la liste des utilisateurs

---
+++ src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $users = $entityManager
+            ->getRepository(User::class)
+            ->findBy([], ['registerDate'=>'desc']);
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }   
+}
