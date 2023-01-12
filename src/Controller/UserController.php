093288491bedabe86e84f2e36939e50095bc29a9
Mael Jansen <maeljansen2003@gmail.com>
2023-01-12 11:28:40 +0100

    Suppression du code mort

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
-use Knp\Component\Pager\PaginatorInterface; 
-use PhpParser\Node\Expr\Cast\Int_;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
