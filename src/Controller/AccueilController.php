abc4910f7772c5fb898c49a0722cea31c6ef7db5
Pascal <arthur.pascal@etu.u-bordeaux.fr>
2023-01-12 15:27:55 +0100

    gestion des doublons dans l'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -18,8 +18,12 @@ class AccueilController extends AbstractController
         $repository = $em->getRepository(Series::class);
         $id=$repository->getAllId();
         $dixId = [];
-        for($i=0;$i<10;$i++){
-            array_push($dixId,$id[rand(0,count($id)-1)]);
+        while(count($dixId)<10){
+            $nb = rand(0,count($id)-1); // random pour selectionner aléatoirement un id
+            $row =$id[$nb]; // la ligne concernée par le random
+            $colonne = $row['id']; // uniquement l'id 
+            array_push($dixId,$colonne); // on ajoute l'id
+            $dixId = array_unique($dixId); // on supprime les doublons
         }
         $dixSeries=$repository->findDixSeries($dixId[0],$dixId[1],$dixId[2],$dixId[3],$dixId[4],$dixId[5],$dixId[6],$dixId[7],$dixId[8],$dixId[9]);
         return $this->render('accueil/index.html.twig', [
